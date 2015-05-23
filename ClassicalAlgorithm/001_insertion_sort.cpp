899f7642423950b2aad2338294a6d603c6931e5a
foolonhill <luoyy13@gmail.com>
2015-05-23 17:32:48 +0800

    Update 001_insertion_sort.cpp

--- ClassicalAlgorithm/001_insertion_sort.cpp
+++ ClassicalAlgorithm/001_insertion_sort.cpp
@@ -1,11 +1,19 @@
+#include <iostream>
 
-
-
+int main()
+{    
+    int a[] = {2, 5, 4, 8, 1, 3, 6, 7, 9};
+    
+    InsertSort2(a, 9);
+    
+    for (int i = 0; i < 9; i++ )
+        std::cout << a[i] << std::endl;
+}
 
 /**
 /* my first version:
 /*    a direct solution, 
-/*
+*/
 void InsertSort(int a[], int n)
 {
     int tmp;
@@ -24,7 +32,7 @@ void InsertSort(int a[], int n)
 /**
 /* an improved version:
 /*      when a[j+1] > a[j], we should break the inner loop
-/*
+*/
 void InsertSort1(int a[], int n)
 {
     int tmp;
@@ -45,18 +53,18 @@ void InsertSort1(int a[], int n)
 /*     we needn't swap each time, because we find tmp is 
 /*  assigned the same value: a[i], so we put it outer the
 /*  inner loop, where we just move the value
-/* 
+*/ 
 void InsertSort2(int a[], int n)
 {
-	int i, j;
-	for (i = 1; i < n; i++) {
-		int tmp = a[i];
-		for (j = i; j > 0; j--) {
-			if (tmp < a[j-1])
-				a[j] = a[j-1];
-			else
-				break;
-		}
-		a[j] = tmp;
+    int i, j;
+    for (i = 1; i < n; i++) {
+	int tmp = a[i];
+	for (j = i; j > 0; j--) {
+            if (tmp < a[j-1])
+		a[j] = a[j-1];
+	    else
+		break;
 	}
+	a[j] = tmp;
+    }
 }
