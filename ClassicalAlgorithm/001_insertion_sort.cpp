5362cc141521aedd6cf7a364f68b081a268479dc
foolonhill <luoyy13@gmail.com>
2015-05-23 17:30:41 +0800

    Update and rename 001_insertion_sort to 001_insertion_sort.cpp

--- ClassicalAlgorithm/001_insertion_sort.cpp
+++ ClassicalAlgorithm/001_insertion_sort.cpp
@@ -3,7 +3,8 @@
 
 
 /**
-/* my first version
+/* my first version:
+/*    a direct solution, 
 /*
 void InsertSort(int a[], int n)
 {
@@ -24,7 +25,7 @@ void InsertSort(int a[], int n)
 /* an improved version:
 /*      when a[j+1] > a[j], we should break the inner loop
 /*
-void InsertSort(int a[], int n)
+void InsertSort1(int a[], int n)
 {
     int tmp;
     for (int i = 1; i < n; i++) {
@@ -37,4 +38,25 @@ void InsertSort(int a[], int n)
                 break;
         }
     }
+}
+
+/**
+/* a better solution:
+/*     we needn't swap each time, because we find tmp is 
+/*  assigned the same value: a[i], so we put it outer the
+/*  inner loop, where we just move the value
+/* 
+void InsertSort2(int a[], int n)
+{
+	int i, j;
+	for (i = 1; i < n; i++) {
+		int tmp = a[i];
+		for (j = i; j > 0; j--) {
+			if (tmp < a[j-1])
+				a[j] = a[j-1];
+			else
+				break;
+		}
+		a[j] = tmp;
+	}
 }
