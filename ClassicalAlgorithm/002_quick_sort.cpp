34f3f0e9d210a6e7d4cfe159006c763a09719d46
foolonhill <luoyy13@gmail.com>
2015-05-24 18:31:18 +0800

    Update 002_quick_sort.cpp

--- ClassicalAlgorithm/002_quick_sort.cpp
+++ ClassicalAlgorithm/002_quick_sort.cpp
@@ -87,3 +87,46 @@ void QuickSort2(int a[], int low, int high)
 		QuickSort2(a, i+1, high);
 	}
 }
+/*
+**  combined and non recursive version
+*/
+void NonRecursiveQuickSort1(int a[], int low, int high)
+{
+	stack<int> st;
+
+	if (low < high) {
+		st.push(low);
+		st.push(high);
+
+		while (!st.empty()) {
+			int right = st.top();
+			st.pop();
+			int left = st.top();
+			st.pop();
+
+			int start = left;
+			int end = right;
+			int pivot = a[start];
+			while (start < end) {
+				while (start < end && a[end] >= pivot)
+					end--;
+				a[start] = a[end];
+
+				while (start < end && a[start] < pivot)
+					start++;
+				a[end] = a[start];
+			}
+			a[start] = pivot;
+
+			if (left < start -1 ) {
+				st.push(left);
+				st.push(start-1);
+			}
+
+			if (right > start + 1) {
+				st.push(start+1);
+				st.push(right);
+			}
+		}
+	}
+}
