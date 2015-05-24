214d1ea1f53b33fa208179a41d0631fd13b74ead
foolonhill <luoyy13@gmail.com>
2015-05-24 18:01:38 +0800

    Update 002_quick_sort.cpp

--- ClassicalAlgorithm/002_quick_sort.cpp
+++ ClassicalAlgorithm/002_quick_sort.cpp
@@ -1,9 +1,36 @@
 
-
 /*
-** a simple partition function
+** a simple partition function:
+*     one direction
 */
 int Partition1(int a[], int low, int high)
+{
+	if (low >= high)
+		return -1;
+
+	int rol = a[low];
+	int m = low;
+	int tmp;
+	for (int i = low+1; i <= high; i++) {
+		if (a[i] < rol) {
+			tmp = a[++m];
+			a[m] = a[i];
+			a[i] = tmp;
+		}
+	}
+
+	tmp = a[m];
+	a[m] = rol;
+	a[low] = tmp;
+
+	return m;
+}
+
+/*
+** a better(mayebe) partition function:
+**       two direction
+*/
+int Partition2(int a[], int low, int high)
 {
 	int rol = a[low];
 	
@@ -30,5 +57,33 @@ void QuickSort1(int a[], int low, int high)
 		int i = Partition1(a, low, high);
 		QuickSort1(a, low, i-1);
 		QuickSort1(a, i+1, high);
+	}
+}
+
+
+/*
+** recursive and combined version
+*/
+void QuickSort2(int a[], int low, int high)
+{
+	if (low < high) {
+		int rol = a[low];
+		int i, j;
+		i = low;
+		j = high;
+		while (i < j) {
+			while (i < j && a[j] >= rol)
+				j--;
+			a[i] = a[j];
+
+			while (i < j && a[i] < rol)
+				i++;
+			a[j] = a[i];
+		}
+
+		a[i] = rol;
+
+		QuickSort2(a, low, i-1);
+		QuickSort2(a, i+1, high);
 	}
 }
