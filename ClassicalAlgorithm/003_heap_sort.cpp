7ca933b07a143a844d4da7d537a4c8d2b2e4ea77
foolonhill <luoyy13@gmail.com>
2015-05-24 22:18:25 +0800

    Create 003_heap_sort.cpp

---
+++ ClassicalAlgorithm/003_heap_sort.cpp
@@ -0,0 +1,33 @@
+// adjust to max heap
+void HeapAdjust(int a[], int s, int e)
+{
+	int tmp = a[s];
+	
+	for (int j = 2 * s; j <= e; j *= 2) {
+		if (j < e && a[j] < a[j+1])
+			j++;
+		if (tmp > a[j])
+			break;
+		a[s] = a[j];
+		s = j;
+	}
+	a[s] = tmp;
+}
+
+
+void HeapSort(int a[], int len)
+{
+	// create the heap
+	for (int i = len/2; i >= 1; i--)
+		HeapAdjust(a, i, len);
+
+	int tmp;
+	// swap the maximum to the end and adjust the heap
+	for (int i = len; i >= 2; i--) {
+		tmp = a[1];
+		a[1] = a[i];
+		a[i] = tmp;
+
+		HeapAdjust(a, 1, i-1);
+	}
+}
