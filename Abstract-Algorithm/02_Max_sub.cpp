14916b124e00eafb8fd15d489ccb566941554fff
foolonhill <luoyy13@gmail.com>
2015-05-27 10:31:28 +0800

    Update 02_Max_sub.cpp

--- Abstract-Algorithm/02_Max_sub.cpp
+++ Abstract-Algorithm/02_Max_sub.cpp
@@ -20,9 +20,11 @@ int MaxSub1(int a[], int n)
 	int max_here = 0;
 
 	for (int i = 1; i < n; i++) {
+		// 得到当前元素之前所有元素的最大和，保证每一阶段大于零
 		max_ending_here += a[i-1];
 		max_ending_here = max(max_ending_here, 0);
-
+		
+		// 加上当前元素后，再取较大值
 		max_here = max_ending_here + a[i];
 		max_here = max(max_here, max_ending_here);   
 	}
