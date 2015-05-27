96533ab73a318cb65768c4a9aea86d8cc67d5796
foolonhill <luoyy13@gmail.com>
2015-05-27 10:24:42 +0800

    Create 02_Max_sub.cpp

---
+++ Abstract-Algorithm/02_Max_sub.cpp
@@ -0,0 +1,26 @@
+int max(int a, int b)
+{
+	return a >= b ? a : b;
+}
+
+int MaxSub1(int a[], int n)
+{
+	if (n <= 0)
+		return 0;
+	
+	if (n == 1)
+		return a[0];
+
+	int max_ending_here = 0;
+	int max_here = 0;
+
+	for (int i = 1; i < n; i++) {
+		max_ending_here += a[i-1];
+		max_ending_here = max(max_ending_here, 0);
+
+		max_here = max_ending_here + a[i];
+		max_here = max(max_here, max_ending_here);   
+	}
+
+	return max_here;
+}
