9f198ae9097f86cfa4d7bca7693c7d8426f5a7e2
foolonhill <luoyy13@gmail.com>
2015-05-31 20:06:52 +0800

    Update 02_strstr.c

--- CStringAlgorithm/02_strstr.c
+++ CStringAlgorithm/02_strstr.c
@@ -1,9 +1,14 @@
 /**
-*
-*/
+ * Description:
+ * 		find sub_str in str. If found, return the address of first match. If not, return NULL.
+ * 
+ * There are two versions of strstr()：
+ * 	1) char* strstr(char *str, const char *sub_str)
+ * 	2) const char* strstr(const char *str, const char *sub_str)
+ */
 
 
-char* my_strstr(const char *sub_str, char *str)
+char* my_strstr(char *str, const char *sub_str)
 {
 	if (sub_str == NULL || str == NULL)
 		return NULL;
@@ -26,8 +31,6 @@ char* my_strstr(const char *sub_str, char *str)
 		} else {
 			tmp = sub_str;
 		}
-
 	}
-
 	return NULL;
 }
