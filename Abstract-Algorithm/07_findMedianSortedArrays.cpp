4cd480d82e0da26254931e17a8b65a392644dac3
foolonhill <luoyy13@gmail.com>
2015-09-26 12:10:58 +0800

    Create 07_findMedianSortedArrays.cpp

---
+++ Abstract-Algorithm/07_findMedianSortedArrays.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+	/**
+	 * O(m + n) time complexity
+	 *
+	 */
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        size_t len1 = nums1.size();
+        size_t len2 = nums2.size();
+        size_t len = len1 + len2;
+        vector<int> comb(len, 0);
+        
+        size_t i = 0, j = 0, k = 0;
+        for (k = 0; k < len && i < len1 && j < len2; ++k) {
+            if (nums1[i] < nums2[j]) {
+                comb[k] = nums1[i];
+                i++;
+            } else {
+                comb[k] = nums2[j];
+                j++;
+            }
+        }
+        if (i < len1) {
+            for (;k < len && i < len1; ++k)
+                comb[k] = nums1[i++];
+        }
+        if (j < len2) {
+            for (;k < len && j < len2; ++k)
+                comb[k] = nums2[j++];
+        }
+        
+        if ((len) % 2 != 0 )
+            return comb[(len) / 2];
+        else
+            return (comb[len / 2] + comb[len/2 -1]) / 2.0;    // use 2.0 is very important
+        
+    }
+};
